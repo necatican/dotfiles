a97b445a1dda67e2d22b32afd18c655d742f87a2
necatican <necaticanyildirim@gmail.com>
2022-03-08 09:59:58 +0300

    Add change-extension function

--- dotfiles/zsh/functions/change-extension.sh
+++ dotfiles/zsh/functions/change-extension.sh
@@ -1,13 +1,16 @@
-# source https://github.com/thoughtbot/dotfiles/blob/main/zsh/functions/change-extension
+# Original source https://github.com/thoughtbot/dotfiles/blob/main/zsh/functions/change-extension
+# I edited this to add a third argument to specify another directory and work with bash when needed
 
 # Change file extensions recursively in current directory or $3rd argument
 #
 #   change-extension erb haml
 
-function change-extension() {
+change-extension () {
   _path=${3-$(pwd)}
 
-  foreach f (${_path%/}/**/*.$1)
-    mv $f $f:r.$2 && echo "Changed $f to $f:r.$2"
-  end
+  for f in "${_path%/}"/**/*."$1"
+  do
+    _newPath=${f%".$1"}.$2
+    mv "$f" "$_newPath" && echo "Changed $f to $_newPath"
+  done
 }
