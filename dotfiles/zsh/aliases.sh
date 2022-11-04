16bfd6446286d57d5a3e5410dba9e38f1fe36e47
necatican <contact@necatican.com>
2022-11-04 21:47:42 +0300

    chore(brew): replace lima with podman
    Signed-off-by: necatican <contact@necatican.com>

--- dotfiles/zsh/aliases.sh
+++ dotfiles/zsh/aliases.sh
@@ -10,7 +10,3 @@ alias ls='ls --color=auto'
 alias la='ls -lah'
 alias grep='grep --color=auto'
 alias mkdir='mkdir -pv'
-
-# Docker | https://medium.com/nttlabs/containerd-and-lima-39e0b64d2a59
-alias docker='start_limactl_if_not_started' # My brain is hardwired to type docker in certain situations. :(
-alias lnc='start_limactl_if_not_started'
