dadf8e78b864e69536fd1a47fcf18fc87f71d08a
necatican <contact@necatican.com>
2025-02-24 02:01:08 +0300

    feat: modify the brewfile
    Signed-off-by: necatican <contact@necatican.com>

--- dotfiles/zsh/aliases.sh
+++ dotfiles/zsh/aliases.sh
@@ -3,3 +3,8 @@ alias ls='ls --color=auto'
 alias la='ls -lah'
 alias grep='grep --color=auto'
 alias mkdir='mkdir -pv'
+
+# Python
+alias pp='poetry run python'
+alias psh='poetry shell'
+alias pa='poetry add'
